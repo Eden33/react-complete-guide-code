f855aa1d123931dead9980150d0c98a3955008ea
Eden33 <eduard.gopp@gmail.com>
2023-09-29 23:04:03 +0200

    Assignment 4 solution

--- code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
+++ code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isAddNewExpense, setIsAddNewExpense] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsAddNewExpense(false);
   };
+
+  const onCancelButtonClickedHandler = () => {
+    setIsAddNewExpense(false);
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {isAddNewExpense && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancelButtonClicked={onCancelButtonClickedHandler}/>
+      )}
+      {!isAddNewExpense && (
+        <button onClick={() => setIsAddNewExpense(true)}>Add New Expense</button>
+      )}
     </div>
   );
 };
