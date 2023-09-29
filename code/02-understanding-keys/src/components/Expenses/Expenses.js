1b7b916099793c8bf4a501969403d24cd7613093
Eden33 <eduard.gopp@gmail.com>
2023-09-29 21:58:30 +0200

    Assignment 3 solution

--- code/02-understanding-keys/src/components/Expenses/Expenses.js
+++ code/02-understanding-keys/src/components/Expenses/Expenses.js
@@ -12,6 +12,12 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpensesByYear = (expenses) => {
+    return expenses.filter(
+      (expense) => expense.date.getFullYear().toString() === filteredYear
+    );
+  };
+
   return (
     <div>
       <Card className='expenses'>
@@ -19,7 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filterExpensesByYear(props.items).map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
